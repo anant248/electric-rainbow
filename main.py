98cb77e04c1432fbbf595462787758db57db27a1
Anant Goyal <anantgoyal2000@gmail.com>
2023-01-30 13:33:52 -0800

    initial GPIO code to main python

--- main.py
+++ main.py
@@ -1,25 +1,47 @@
+# Libraries
 import msgpack
+import RPi.GPIO as GPIO
 import os
 import time
 import socket
 import threading
 from random import *
 
-pausePlayButton = 1 # 0 means play, 1 means pause
-clearButton = 1 # 1 means clear
+# GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+
+# set GPIO Pins
+pin23 = 23 # GPIO23 on the Pi
+pin24 = 24 # GPIO24 on the Pi
+pin25 = 25 # GPIO25 on the Pi
+
+# set GPIO direction (IN / OUT)
+GPIO.setup(pin23, GPIO.IN)
+GPIO.setup(pin24, GPIO.IN)
+GPIO.setup(pin25, GPIO.IN)
+
+# assign pin inputs to their respective button readings
+pausePlayButton = GPIO.input(pin23)
+clearButton = GPIO.input(pin24)
+screenshotButton = GPIO.input(pin25)
+
+# pausePlayButton = 1 # 0 means play, 1 means pause
+# clearButton = 1 # 1 means clear
 
 def clearCanvas():
     global clearButton
 
     while True:
         if (clearButton == 1): 
-            clearButton = 0
+            # clearButton = 0
+            print("Clear button is 1")
         else:
-            clearButton = 1
-            time.sleep(0.10)
-            clearButton = 0
+            # clearButton = 1
+            # time.sleep(0.10)
+            # clearButton = 0
+            print("Clear Button is 0")
 
-        print(clearButton)
+        # print(clearButton)
         time.sleep(3)
 
 def buttonSwitch():
@@ -27,11 +49,13 @@ def buttonSwitch():
 
     while True:
         if (pausePlayButton == 1): 
-            pausePlayButton = 0
+            # pausePlayButton = 0
+            print("Pause/Play button is 1")
         else:
-            pausePlayButton = 1
+            # pausePlayButton = 1
+            print("Pause/Play button is 0")
 
-        print(pausePlayButton)
+        # print(pausePlayButton)
         time.sleep(10)
 
 def dataSend():
@@ -73,4 +97,5 @@ if __name__ == "__main__":
         p2.join()
         p3.join()
     except KeyboardInterrupt:
+        GPIO.cleanup()
         print("Exiting Python gracefully?")
